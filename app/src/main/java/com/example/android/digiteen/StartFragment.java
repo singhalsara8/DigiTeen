102e8cb875e28e5aef83453dbe3a015efafe2c03
singhalsara8 <singhalsara48@gmail.com>
2019-12-07 21:51:15 +0530

    add navigation component

---
+++ app/src/main/java/com/example/android/digiteen/StartFragment.java
@@ -0,0 +1,36 @@
+package com.example.android.digiteen;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link StartFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link StartFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class StartFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_start, container, false);
+    }
+
+    }
