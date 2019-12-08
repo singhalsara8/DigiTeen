2ccfbf119e3c14736ce2e36c06a95669fa96a8d3
singhalsara8 <singhalsara48@gmail.com>
2019-12-08 19:02:18 +0530

    login fragment

--- app/src/main/java/com/example/android/digiteen/LoginFragment.java
+++ app/src/main/java/com/example/android/digiteen/LoginFragment.java
@@ -2,33 +2,74 @@ package com.example.android.digiteen;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link LoginFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link LoginFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class LoginFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    View view;
+    private EditText memail, mpassword;
+    private Button mloginbtn;
+    private TextView msignin;
+    private FirebaseAuth fAuth;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        view = inflater.inflate(R.layout.fragment_login, container, false);
+
+        memail = view.findViewById(R.id.email_id);
+        mpassword = view.findViewById(R.id.password);
+        mloginbtn = view.findViewById(R.id.login_btn);
+        msignin = view.findViewById(R.id.signUp);
+        fAuth = FirebaseAuth.getInstance();
+
+        mloginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = memail.getText().toString().trim();
+                String password = mpassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email))
+                {
+                    memail.setError("Email is Required");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password))
+                {
+                    mpassword.setError("Password is Required");
+                    return;
+                }
+
+                fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful())
+                        {
+                            Toast.makeText(getContext(),"login successful", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+        
+        return view;
     }
 
-    }
+}
