293eb03a34661b7bb4682e33e8a0c8cd5818f838
singhalsara8 <singhalsara48@gmail.com>
2019-12-26 02:52:56 +0530

    implement delete menu item button

--- app/src/main/java/com/example/android/digiteen/Adapter/OwnerMenuAdapter.java
+++ app/src/main/java/com/example/android/digiteen/Adapter/OwnerMenuAdapter.java
@@ -1,22 +1,35 @@
 package com.example.android.digiteen.Adapter;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.android.digiteen.Model.OwnerMenu;
 import com.example.android.digiteen.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.List;
 
 public class OwnerMenuAdapter extends RecyclerView.Adapter<OwnerMenuAdapter.OwnerMenuViewHolder> {
     private List<OwnerMenu> ownerMenus;
     private Context context;
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference databaseReference, reference;
+    private String ownerbhawan;
+    private String itemName;
 
     public OwnerMenuAdapter(List<OwnerMenu> ownerMenus, Context context) {
         this.ownerMenus = ownerMenus;
@@ -26,14 +39,14 @@ public class OwnerMenuAdapter extends RecyclerView.Adapter<OwnerMenuAdapter.Owne
     @NonNull
     @Override
     public OwnerMenuViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_owner_menu,parent,false);
-        OwnerMenuViewHolder ownerMenuViewHolder=new OwnerMenuViewHolder(view);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_owner_menu, parent, false);
+        OwnerMenuViewHolder ownerMenuViewHolder = new OwnerMenuViewHolder(view);
         return ownerMenuViewHolder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull OwnerMenuViewHolder holder, int position) {
-        final OwnerMenu ownerMenu=ownerMenus.get(position);
+    public void onBindViewHolder(@NonNull OwnerMenuViewHolder holder, final int position) {
+        final OwnerMenu ownerMenu = ownerMenus.get(position);
         holder.itemname.setText(ownerMenu.getItemname());
         holder.itemprice.setText(String.valueOf(ownerMenu.getItemprice()));
     }
@@ -43,12 +56,52 @@ public class OwnerMenuAdapter extends RecyclerView.Adapter<OwnerMenuAdapter.Owne
         return ownerMenus.size();
     }
 
-    public class OwnerMenuViewHolder extends RecyclerView.ViewHolder{
-        private TextView itemname,itemprice;
-        private OwnerMenuViewHolder(View view){
+    public class OwnerMenuViewHolder extends RecyclerView.ViewHolder {
+        private TextView itemname, itemprice;
+        private ImageButton button;
+
+        private OwnerMenuViewHolder(View view) {
             super(view);
-            itemname=view.findViewById(R.id.owner_menu_itemname);
-            itemprice=view.findViewById(R.id.owner_menu_itemprice);
+            itemname = view.findViewById(R.id.owner_menu_itemname);
+            itemprice = view.findViewById(R.id.owner_menu_itemprice);
+            button = view.findViewById(R.id.owner_clear_menuitem);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                    builder.setTitle("CONFIRMATION");
+                    builder.setMessage("Do you want to remove item from menu?");
+                    builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            firebaseAuth = FirebaseAuth.getInstance();
+                            databaseReference = FirebaseDatabase.getInstance().getReference();
+                            reference = FirebaseDatabase.getInstance().getReference();
+                            reference = databaseReference.child("user").child(firebaseAuth.getCurrentUser().getUid()).child("profile").child("bhawan");
+                            reference.addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                    ownerbhawan = dataSnapshot.getValue(String.class);
+                                    itemName = ownerMenus.get(getAdapterPosition()).getItemname();
+                                    databaseReference.child("bhawan").child(ownerbhawan).child("Menu").child(itemName).removeValue();
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                }
+                            });
+                        }
+                    });
+                    builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                        }
+                    });
+                    builder.show();
+                }
+            });
         }
 
     }
